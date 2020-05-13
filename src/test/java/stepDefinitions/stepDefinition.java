41c967966baafd112977f4f3a7861deed6522939
anariky1 <ananthgithub@gmail.com>
2020-05-13 15:30:41 -0500

    second change from other user

--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -62,7 +62,6 @@ public class stepDefinition extends Utils {
 		System.out.println("git add");
 		System.out.println("git add");
 		System.out.println("git add");
-		System.out.println("newly added");
 		ResponseSpecification= new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON). build();
 		   if(method.equals("POST")){
 		      response=res.when()
